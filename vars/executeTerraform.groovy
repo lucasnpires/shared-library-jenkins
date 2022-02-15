22012568a00f05a69a5c17f9b7423fc32ecb09ba
Lucas Neves Pires <954058@lojasrenner.com.br>
2022-02-15 12:43:27 -0300

    ajuste terraform pipeline

--- vars/executeTerraform.groovy
+++ vars/executeTerraform.groovy
@@ -6,5 +6,18 @@ def call(Map config = [:], String comandoTerraform) {
         sh "terraform --version"
     } else if(comandoTerraform.equals('init')){
         sh "echo terraform init"
+    } else if(comandoTerraform.equals('fmt')){
+        sh "echo terraform fmt"
+    } else if(comandoTerraform.equals('validate')){
+        sh "echo terraform validate"
+    } else if(comandoTerraform.equals('plan')){
+        sh "echo terraform plan"
+    } else if(comandoTerraform.equals('apply')){
+        sh "echo terraform apply"
+    } else if(comandoTerraform.equals('destroy')){
+        sh "echo terraform destroy"
+    } else {
+        sh "command not permited. Use init, fmt, validate, plan, apply or destroy"
     }
+}
 }
