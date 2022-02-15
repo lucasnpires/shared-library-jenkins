62ac6769896461d4388c58d62e5ac89f3f209f1c
Lucas Neves Pires <954058@lojasrenner.com.br>
2022-02-15 12:38:58 -0300

    ajuste terraform pipeline

--- vars/executeTerraform.groovy
+++ vars/executeTerraform.groovy
@@ -1,7 +1,7 @@
 def call(Map config = [:], String comandoTerraform) {
     sh "echo Cloud: ${config.cloud}"
-    sh "echo Command: ${config.comandoTerraform}"
-    
+    sh "echo Command: ${comandoTerraform}"
+
     if(comandoTerraform.equals('version')){
         sh "terraform --version"
     } else if(comandoTerraform.equals('init')){
