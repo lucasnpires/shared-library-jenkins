c999e007899fff877b783f532e03a1207578cf97
Lucas Neves Pires <954058@lojasrenner.com.br>
2022-02-15 14:27:25 -0300

    ajuste terraform pipeline

--- vars/executeTerraform.groovy
+++ vars/executeTerraform.groovy
@@ -1,15 +1,28 @@
 def call(Map config = [:], String comandoTerraform) {
     def dirExecucao = "terraform/${config.cloud}/${config.resourceType}/${config.projectName}"
 
-    sh "echo Cloud: ${config.cloud}"
-    sh "echo resourceType: ${config.resourceType}"
-    sh "echo projectName: ${config.projectName}"
-    sh "echo dirExecucao: ${dirExecucao}"
+    sh """
+        echo Cloud: ${config.cloud}
+        echo ResourceType: ${config.resourceType}
+        echo Nome Projeto: ${config.projectName}
+        echo Diretório de Execucão: ${dirExecucao}
+    """
 
     if(comandoTerraform.equals('version')){
         sh "terraform --version"
-    } else if(comandoTerraform.equals('init') || comandoTerraform.equals('fmt') || comandoTerraform.equals('validate') || comandoTerraform.equals('plan') || comandoTerraform.equals('apply') || comandoTerraform.equals('destroy')){
-        sh "cd ${dirExecucao} && ls -lha && terraform ${comandoTerraform}"
+    } else if(comandoTerraform.equals('init')){
+        withCredentials([file(credentialsId: 'rsa_git_devops', variable: 'FILE')]) {
+            sh """
+                cat $FILE
+                cd $dirExecucao && ls -lha && echo terraform $comandoTerraform
+            """                        
+        }
+    } else if(comandoTerraform.equals('fmt') || comandoTerraform.equals('validate') || comandoTerraform.equals('plan') || comandoTerraform.equals('apply') || comandoTerraform.equals('destroy')){
+        sh """
+            cd ${dirExecucao}
+            ls -lha 
+            terraform ${comandoTerraform}
+        """
     } else {
         sh "echo command not permited. Use init, fmt, validate, plan, apply or destroy"
     }
