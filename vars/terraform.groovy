844d7c220280a25db177137e895ccf26c1b26588
Lucas Neves Pires <954058@lojasrenner.com.br>
2022-02-14 23:47:47 -0300

    add terraform script

---
+++ vars/terraform.groovy
@@ -0,0 +1,30 @@
+def call(String command) {
+  if (command == 'init') {
+    pipeline {
+        agent {
+            kubernetes {
+                yaml '''
+                apiVersion: v1
+                kind: Pod
+                spec:
+                containers:
+                - name: alpine
+                    image: alpine
+                    command:
+                    - cat
+                    tty: true
+                '''
+            }
+        }
+        stages {
+            stage('Terraform Init') {
+                steps {
+                    container('alpine') {          
+                        sh 'echo terraform init'
+                    }
+                }
+            }
+        }
+    }
+  }
+}
