1e8f763be2e649dc70b63441aa469b58abd50f9a
Lucas Neves Pires <954058@lojasrenner.com.br>
2022-02-16 14:53:07 -0300

    ajuste script getApproval

--- vars/getInputApprovalDestroy.groovy
+++ vars/getInputApprovalDestroy.groovy
@@ -11,6 +11,8 @@ def call (){
                 )
             ]
         )
-        return approvalDestroyInput
+
+        sh "echo $approvalDestroyInput"
+        //return approvalDestroyInput
     }    
 }
