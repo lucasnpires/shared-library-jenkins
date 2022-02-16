05bd14a550c33375102ea820ab41d19a279a97d7
Lucas Neves Pires <954058@lojasrenner.com.br>
2022-02-15 23:01:51 -0300

    ajustes terraform script

--- vars/executeTerraform.groovy
+++ vars/executeTerraform.groovy
@@ -1,35 +1,25 @@
 def call(Map config = [:], String comandoTerraform) {
     def dirExecucao = "terraform/${config.cloud}/${config.resourceType}/${config.projectName}"
 
-    sh """
-        echo Cloud: $config.cloud
-        echo ResourceType: $config.resourceType
-        echo Nome Projeto: $config.projectName
-        echo Diretório de Execucão: $dirExecucao
-    """
-
     if(comandoTerraform.equals('version')){
         sh "terraform --version"
     } 
     
     else if(comandoTerraform.equals('init')){
-        //withCredentials([file(credentialsId: 'rsa-azdevops-git', variable: 'FILE')]) {
-            //mkdir -p ~/.ssh
-            //cp -ru "$FILE" ~/.ssh/id_rsa
-            //echo '' > ~/.ssh/authorized_keys
-            //export  GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa"
-            
-            sh """
-                cd $dirExecucao && ls -lha && terraform $comandoTerraform
-            """                        
-        //}
+        sh """
+            echo Cloud: $config.cloud
+            echo ResourceType: $config.resourceType
+            echo Nome Projeto: $config.projectName
+            echo Diretório de Execucão: $dirExecucao
+
+            cd $dirExecucao && terraform $comandoTerraform
+        """
     } 
     
     else if(comandoTerraform.equals('fmt') || comandoTerraform.equals('validate') || comandoTerraform.equals('plan') || comandoTerraform.equals('apply') || comandoTerraform.equals('destroy')){
         sh """
-            cd ${dirExecucao}
-            ls -lha 
-            echo terraform ${comandoTerraform}
+            cd ${dirExecucao}          
+            terraform ${comandoTerraform}
         """
     } else {
         sh "echo command not permited. Use init, fmt, validate, plan, apply or destroy"
