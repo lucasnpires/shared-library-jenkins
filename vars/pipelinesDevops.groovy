efa764715f52ce55b0246663da4ee79391b90d43
Lucas Neves Pires <954058@lojasrenner.com.br>
2022-02-16 16:29:01 -0300

    add script pipelineDevops

---
+++ vars/pipelinesDevops.groovy
@@ -0,0 +1,81 @@
+def call(Map config = [:]){
+    pipeline {
+        agent {
+            kubernetes {
+                yaml libraryResource('terraform-pod-template.yaml')
+            }
+        }
+        stages {
+            stage('Input Values') {
+                steps {
+                    script{
+                        userInput = getInput() 
+                        config.projectName = userInput.ProjectName         
+                        config.cloud = userInput.Cloud
+                        config.resourceType = userInput.ResourceType
+                    }
+                }
+            }
+
+            stage('TF Init') {
+                steps {
+                    container('terraform') { 
+                        //commands to use: version, init, fmt, validate, plan, apply, destroy
+                        executeTerraform(config, 'init')
+                    }
+                }
+            }
+
+            stage('TF Validate') {
+                steps {
+                    container('terraform') {                     
+                        executeTerraform(config, 'validate')
+                        executeTerraform(config, 'fmt')
+                    }
+                }
+            }
+
+            stage('TF Plan') {
+                steps {
+                    container('terraform') {                     
+                        executeTerraform(config, 'plan')                    
+                    }
+                }
+            }
+
+            stage('TF Approval') {
+                steps {
+                    script {              
+                        config.applyDestroy = getInputApprovalDestroy()          
+                    }
+                }
+            }
+
+            stage('TF Apply') {
+                when { 
+                    expression {
+                        config.applyDestroy == 'apply'
+                    }                
+                }            
+                steps {
+                    container('terraform') {                     
+                        executeTerraform(config, 'apply')                    
+                    }
+                }
+            }
+
+            stage('TF Destroy') {
+                when { 
+                    expression {
+                        config.applyDestroy == 'destroy'
+                    }                
+                }            
+                steps {
+                    container('terraform') {                     
+                        executeTerraform(config, 'destroy')                    
+                    }
+                }
+            }
+        }
+    }
+}
