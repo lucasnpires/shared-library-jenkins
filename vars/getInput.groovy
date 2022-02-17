6494c337230d5f736d16c05cb67f26ed67bae3ea
Lucas Neves Pires <954058@lojasrenner.com.br>
2022-02-17 15:52:18 -0300

    ajustes getInputs

--- vars/getInput.groovy
+++ vars/getInput.groovy
@@ -4,42 +4,21 @@ def call (){
             id: 'userInput', 
             message: 'Preencha as informações para executar a pipeline', 
             parameters: [
-                [$class: 'ChoiceParameter',                     
-                    description: 'Select the Environemnt from the Dropdown List', 
-                    filterLength: 1, 
-                    filterable: false, 
-                    name: 'Env', 
-                    script: [
-                        $class: 'GroovyScript', 
-                        fallbackScript: [
-                            classpath: [], 
-                            sandbox: false, 
-                            script: 
-                                "return['Could not get The environemnts']"
-                        ], 
-                        script: [
-                            classpath: [], 
-                            sandbox: false, 
-                            script: 
-                                "return['dev','stage','prod']"
-                        ]
-                    ]
-                ],
-                //choice(
-                //    name: 'Cloud', 
-                //    choices: getClouds(),
-                //    description: 'Cloud'
-                //),                                
-                //choice(
-                //    name: 'ProjectName', 
-                //    choices: getProjects('oci'),
-                //    description: 'Nome do Projeto'
-                //),
-                //choice(
-                //    name: 'ResourceType', 
-                //    choices: ['kubernetes','compute_instance',''],
-                //    description: 'Resource Type'
-                //)
+                choice(
+                    name: 'Cloud', 
+                    choices: getClouds(),
+                    description: 'Cloud'
+                ),                                
+                choice(
+                    name: 'ProjectName', 
+                    choices: getProjects($Cloud),
+                    description: 'Nome do Projeto'
+                ),
+                choice(
+                    name: 'ResourceType', 
+                    choices: ['kubernetes','compute_instance',''],
+                    description: 'Resource Type'
+                )
             ]
         )
 
@@ -60,4 +39,3 @@ def getProjects(String cloud){
     }
     
 }
-
