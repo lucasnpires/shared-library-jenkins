6896d33554d6dad0bb4f7fab2aee98ab77ae3602
Lucas Neves Pires <954058@lojasrenner.com.br>
2022-02-21 12:35:56 -0300

    docker pipeline

--- vars/executeDocker.groovy
+++ vars/executeDocker.groovy
@@ -2,8 +2,7 @@
 def call(Map parametersDocker = [:]) {    
 
     sh """
-        cd $parametersDocker.projectName 
-        ls -lha 
-        echo docker build -t us-phoenix-1.ocir.io/axxalonri6o1/devops/$parametersDocker.projectName:$parametersDocker.version --no-cache -f Dockerfile-$parametersDocker.version .
+        cd $parametersDocker.projectName         
+        docker build -t us-phoenix-1.ocir.io/axxalonri6o1/devops/$parametersDocker.projectName:$parametersDocker.version --no-cache -f Dockerfile-$parametersDocker.version .
     """
 }
