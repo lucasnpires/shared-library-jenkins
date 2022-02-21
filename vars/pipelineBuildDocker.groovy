18ffb67a5bd208f2d3dcd11394f8c81f62e81493
Lucas Neves Pires <954058@lojasrenner.com.br>
2022-02-21 11:05:00 -0300

    docker pipeline

--- vars/pipelineBuildDocker.groovy
+++ vars/pipelineBuildDocker.groovy
@@ -1,10 +1,5 @@
 def call(){
-    pipeline {
-        environment {
-            registry = "us-phoenix-1.ocir.io/axxalonri6o1/devops/teste"
-            registryCredential = 'dockerhub_id'
-            dockerImage = ''
-        }
+    pipeline {       
         agent {
             kubernetes {
                 yaml libraryResource('dind-pod-template.yaml')
@@ -14,8 +9,8 @@ def call(){
         stages {
             stage('Run Docker') {
                 steps {
-                   script {
-                        dockerImage = docker.build registry + ":$BUILD_NUMBER"
+                   container('docker') {
+                        sh 'docker version'
                     }
                 }
             }
