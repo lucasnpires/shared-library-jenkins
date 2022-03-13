b1a46bcb49f382fd422ff011eb2c5c2c38b8e4b2
Lucas Neves Pires <954058@lojasrenner.com.br>
2022-03-12 23:58:51 -0300

    ajustes branchs

---
+++ src/br/com/renner/services/Utilities.groovy
@@ -0,0 +1,9 @@
+class Utilities implements Serializable {
+  def steps
+  
+  Utilities(steps) {this.steps = steps}
+  
+  def mvn(args) {
+    steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
+  }
+}
